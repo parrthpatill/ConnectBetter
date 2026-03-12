ec3832a7653f8cf409704d1e66407c18d54222f2
Parth Patil <parthpatil0512@gmail.com>
2026-03-12 23:12:45 +0530

    added first middlewar

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next){
+
+    const token = req.header("Authorization");
+
+    if(!token){
+        return res.status(401).json("Access denied");
+    }
+
+    try{
+
+        const verified = jwt.verify(
+            token.split(" ")[1],
+            process.env.JWT_SECRET
+        );
+
+        req.user = verified;
+        next();
+
+    }catch(err){
+        res.status(400).json("Invalid Token");
+    }
+}
+
+module.exports = authMiddleware;
